2e94a8f2f7575eb57d151705f7a139fa91551754
dianGrigorov <diangrigorov@gmail.com>
2022-12-26 12:44:58 +0200

    Create 09.SumOfOddNumbers.cs

---
+++ BasicSintaxLab/09.SumOfOddNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _09.SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; n > 0; i++)
+            {
+                if (i %2 != 0)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                    n--;
+                   
+                }
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
