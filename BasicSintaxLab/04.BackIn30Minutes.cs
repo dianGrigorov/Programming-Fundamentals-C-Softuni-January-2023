be2705ae2899bf7ab83ec736501b81dc5281a772
dianGrigorov <diangrigorov@gmail.com>
2022-12-26 12:38:53 +0200

    Create Program.cs

---
+++ BasicSintaxLab/04.BackIn30Minutes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04.BackIn30Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int addMin = minutes + 30;
+
+            if (addMin > 60)
+            {
+                hours++;
+               
+                if (hours >=24)
+                {
+                    hours = 0;
+                }
+            }
+            if (addMin > 60)
+            {
+                addMin -= 60;
+            }
+            Console.WriteLine($"{hours}:{addMin:d2}");
+
+        }
+    }
+}
