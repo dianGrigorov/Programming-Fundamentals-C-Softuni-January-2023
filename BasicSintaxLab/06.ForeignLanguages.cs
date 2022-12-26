abf0904df79d1bd2fffe0569f0c530f95cb95111
dianGrigorov <diangrigorov@gmail.com>
2022-12-26 12:44:36 +0200

    Create 06.ForeignLanguages.cs

---
+++ BasicSintaxLab/06.ForeignLanguages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06.ForeignLanguages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (input == "USA" || input == "England")
+            {
+                Console.WriteLine("English");
+            }
+            else if (input == "Spain" || input == "Mexico" || input == "Argentina")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
