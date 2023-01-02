7b177cf7a8a42ba9f0c83c4467c7fc3e3575f159
dianGrigorov <diangrigorov@gmail.com>
2023-01-02 15:54:02 +0200

    Create 04.PrintAndSum.cs

---
+++ 01.BasicSintaxProgFundExercise/04.PrintAndSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.PrintAndSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = n; i <= m; i++)
+            {
+                Console.Write($"{i} ");
+                sum = sum + i;
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
