ca05b88ddac8e28088939c39a88f4f1a0384141c
dianGrigorov <diangrigorov@gmail.com>
2023-01-02 15:54:04 +0200

    Create 05.LogIn.cs

---
+++ 01.BasicSintaxProgFundExercise/05.LogIn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.LogIn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string logIn = Console.ReadLine();
+            string password = Console.ReadLine();
+            string reversedPass = string.Empty;
+            int cnt = 0;
+            for (int i = logIn.Length -1 ; i >= 0; i--)
+            {
+                reversedPass += logIn[i];
+            }
+            while (password != reversedPass)
+            {
+                cnt++;
+                if (cnt >=4)
+                {
+                    Console.WriteLine($"User {logIn} blocked!");
+                    return;
+                }
+               
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                password = Console.ReadLine();
+            }
+            if (password == reversedPass)
+            {
+                Console.WriteLine($"User {logIn} logged in.");
+            }
+        }
+    }
+}
