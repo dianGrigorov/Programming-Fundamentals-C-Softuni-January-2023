fdd881b32954d0db19246abe9c8e44e4b2ff8b6a
dianGrigorov <diangrigorov@gmail.com>
2023-01-02 15:54:15 +0200

    Create 10.RageExpenses.cs

---
+++ 01.BasicSintaxProgFundExercise/10.RageExpenses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace _10.RageExpenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             // Input
+            int lostGame = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            //Logic
+            int headsetCnt = lostGame / 2;
+            int mouseCnt = lostGame / 3;
+            int keyboardCnt = lostGame / 6;
+            int displayCnt = lostGame / 12;
+
+            double totalCost = headsetCnt * headsetPrice + mouseCnt * mousePrice + 
+                keyboardCnt * keyboardPrice + displayCnt *displayPrice;
+
+            // Output
+            Console.WriteLine($"Rage expenses: {totalCost:f2} lv.");
+        }
+    }
+}
