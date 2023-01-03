efd357f9b9127bab678f003ed406603ac4f90fba
dianGrigorov <diangrigorov@gmail.com>
2023-01-03 16:02:36 +0200

    Create 04.SumOfChars.cs

---
+++ 02.DataTypeAndVariablesExercise/04.SumOfChars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.SumOfChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                char letters = char.Parse(Console.ReadLine());
+                sum += letters;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
