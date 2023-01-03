f770ffeb4cb3c3168cfa9de1a799c8f048e925d9
dianGrigorov <diangrigorov@gmail.com>
2023-01-03 16:02:38 +0200

    Create 05.PrintPartOfTheASCIITable.cs

---
+++ 02.DataTypeAndVariablesExercise/05.PrintPartOfTheASCIITable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05.PrintPartOfTheASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            
+            char character = '\0';
+            for (int i = start; i <= end; i++)
+            {
+               character = Convert.ToChar(i);
+                Console.Write($"{character} ");
+            }
+           
+        }
+    }
+}
