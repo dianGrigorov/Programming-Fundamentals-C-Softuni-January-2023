42afbd1992cc95584d6a0dd9a79aca4123b37c18
dianGrigorov <diangrigorov@gmail.com>
2023-01-03 16:02:32 +0200

    Create 03.Elevator.cs

---
+++ 02.DataTypeAndVariablesExercise/03.Elevator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03.Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte people = sbyte.Parse(Console.ReadLine());
+            sbyte capacity = sbyte.Parse(Console.ReadLine());
+            byte course = 0;
+
+            while (people > 0)
+            {
+                people -= capacity;
+                
+                course++;
+            }
+            Console.WriteLine(course);
+        }
+    }
+}
