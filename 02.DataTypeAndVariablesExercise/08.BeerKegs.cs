ff1781d2a155f3184464dfef8088bd718474fc0b
dianGrigorov <diangrigorov@gmail.com>
2023-01-04 13:35:40 +0200

    Create 08.BeerKegs.cs

---
+++ 02.DataTypeAndVariablesExercise/08.BeerKegs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxVolume = 0;
+            string maxType = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                string modelOfKeg = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+                if (maxVolume < volume)
+                {
+                    maxVolume = volume;
+                    maxType = modelOfKeg;
+                }
+            }
+            Console.WriteLine(maxType);
+        }
+    }
+}
