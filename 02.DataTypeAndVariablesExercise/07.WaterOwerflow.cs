b1b5eb0c184d8a70dfd78b3f3669d5c8df488a67
dianGrigorov <diangrigorov@gmail.com>
2023-01-04 13:35:36 +0200

    Create 07.WaterOwerflow.cs

---
+++ 02.DataTypeAndVariablesExercise/07.WaterOwerflow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07.WaterOwerflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int tank = 255;
+            int pouredLiters = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                if (liters <= tank)
+                {
+                    tank -= liters;
+                    pouredLiters += liters;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+
+            }
+            Console.WriteLine(pouredLiters);
+
+
+        }
+    }
+}
