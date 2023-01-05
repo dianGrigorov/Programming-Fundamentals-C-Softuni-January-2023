ad7ae9b7fcb92a0fee11bb300407f216b840575d
dianGrigorov <diangrigorov@gmail.com>
2023-01-05 20:42:46 +0200

    Create 10.Pokemon.cs

---
+++ 02.DataTypeAndVariablesExercise/10.Pokemon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Security;
+
+namespace _10.Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int target = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int pokeCnt = 0;
+            int startPower = power;
+
+            while (power >= target)
+            {
+                pokeCnt++;
+                power -= target;
+                if (power != 0 && startPower != 0 && exhaustionFactor != 0)
+                {
+                    if (startPower / (double)power == 2)
+                    {
+                        power = power / exhaustionFactor;
+                    }
+                }
+
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(pokeCnt);
+
+
+        }
+    }
+}
