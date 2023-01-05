49d9eff2f69b6beb00aff5b728720b3b46e2e7b0
dianGrigorov <diangrigorov@gmail.com>
2023-01-05 19:51:29 +0200

    Create 09.SpiceMustFlow.cs

---
+++ 02.DataTypeAndVariablesExercise/09.SpiceMustFlow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+namespace _09.SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint spice = uint.Parse(Console.ReadLine());
+            uint daysCnt = 0;
+            BigInteger produseSpice = 0;
+            uint consume = 26;
+
+            while (spice >= 100)
+            {
+                daysCnt++;
+                produseSpice += (BigInteger)spice - consume;
+                spice -= 10;
+
+            }
+            produseSpice -= consume;
+            if (spice <= consume)
+            {
+                produseSpice = 0;
+            }
+            Console.WriteLine(daysCnt);
+            Console.WriteLine(produseSpice);
+        }
+    }
+}
