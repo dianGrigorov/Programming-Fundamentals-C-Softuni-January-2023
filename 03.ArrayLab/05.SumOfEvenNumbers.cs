6eb8e22f2c3188c3c18065a01d13dc36da1950c5
dianGrigorov <diangrigorov@gmail.com>
2023-01-07 19:14:40 +0200

    Create 05.SumOfEvenNumbers.cs

---
+++ 03.ArrayLab/05.SumOfEvenNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SumOfEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] % 2 == 0)
+                {
+                    sum += num[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
