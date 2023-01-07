aa00f95f3a56e26bff182607b1d3547e5a81bde9
dianGrigorov <diangrigorov@gmail.com>
2023-01-07 19:14:35 +0200

    Create 04.ReverseArrayOfString.cs

---
+++ 03.ArrayLab/04.ReverseArrayOfString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ReverseArrayOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string values = Console.ReadLine();
+            string[] arr = values.Split();
+            string[] reversedString = new string[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                reversedString[i] = arr[arr.Length - 1 - i];
+                Console.Write(reversedString[i] + " ");
+            }
+        }
+    }
+}
