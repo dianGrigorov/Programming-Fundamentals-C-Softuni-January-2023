17e1bfb0e47f6e0ab5012b2a8a7f6c8ab1d7ca06
dianGrigorov <diangrigorov@gmail.com>
2023-01-07 18:25:00 +0200

    Create 03.RoundingNumber.cs

---
+++ 03.ArrayLab/03.RoundingNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.RoundingNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numb = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            int[] roundedNum = new int[numb.Length];
+            for (int i = 0; i < numb.Length; i++)
+            {
+                roundedNum[i] = (int)Math.Round(numb[i], MidpointRounding.AwayFromZero);
+            }
+            for (int i = 0; i < roundedNum.Length; i++)
+            {
+                Console.WriteLine(numb[i] + " => " + roundedNum[i]);
+            }
+        }
+    }
+}
