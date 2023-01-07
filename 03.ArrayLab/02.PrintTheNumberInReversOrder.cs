ff458e5dac3cf0466c950dd3e625650516d6d71e
dianGrigorov <diangrigorov@gmail.com>
2023-01-07 18:24:58 +0200

    Create 02.PrintTheNumberInReversOrder.cs

---
+++ 03.ArrayLab/02.PrintTheNumberInReversOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.PrintTheNumberInReversOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int j = num.Length -1; j >= 0; j--)
+            {
+                Console.Write(num[j] + " ");
+            }
+        }
+    }
+}
