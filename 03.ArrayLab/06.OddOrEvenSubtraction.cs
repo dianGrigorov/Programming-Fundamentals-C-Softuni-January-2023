54cd89026e4bfefca6d31dc2ed788c53800d40cc
dianGrigorov <diangrigorov@gmail.com>
2023-01-08 17:32:53 +0200

    Create 06.OddOrEvenSubtraction.cs

---
+++ 03.ArrayLab/06.OddOrEvenSubtraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.OddOrEvenSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int oddSum = 0;
+            int evenSum = 0;
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                if (firstArr[i] % 2 == 1)
+                {
+                    oddSum += firstArr[i];
+                }
+                else
+                {
+                    evenSum += firstArr[i];
+                }
+            }
+            Console.WriteLine(evenSum - oddSum);
+            
+        }
+    }
+}
