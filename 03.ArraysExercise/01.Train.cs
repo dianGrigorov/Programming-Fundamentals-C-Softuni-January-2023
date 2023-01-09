c862d552bb80dc1db326cc666e8b3dcf600ad3a8
dianGrigorov <diangrigorov@gmail.com>
2023-01-09 17:17:24 +0200

    Create 01.Train.cs

---
+++ 03.ArraysExercise/01.Train.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vagons = int.Parse(Console.ReadLine());
+            int[] train = new int[vagons];
+            int sum = 0;
+
+            for (int i = 0; i < train.Length; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                train[i] = people;
+                sum += people;
+            }
+            Console.WriteLine(string.Join((" "), train));
+            Console.WriteLine(sum);
+        }
+    }
+}
