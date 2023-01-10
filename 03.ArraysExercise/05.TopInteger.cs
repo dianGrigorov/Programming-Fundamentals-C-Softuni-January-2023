d1d5a278d8516ef2aa3dad599a068e238501a717
dianGrigorov <diangrigorov@gmail.com>
2023-01-10 13:23:10 +0200

    Create 05.TopInteger.cs

---
+++ 03.ArraysExercise/05.TopInteger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.TopInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arry = Console.ReadLine()
+                 .Split(" ")
+                 .Select(int.Parse)
+                 .ToArray();
+
+           
+
+            for (int i = 0; i < arry.Length; i++)
+            {
+                int curentNum = arry[i];
+                bool isTop = true;
+               
+                for (int j = i +1; j < arry.Length; j++)
+                {
+                    int nextNum = arry[j];
+                    if (nextNum >= curentNum)
+                    {
+                        isTop = false;
+                        break;
+                    }
+                }
+
+                if (isTop)
+                {
+                    Console.Write(curentNum + " ");
+                }
+            }
+
+        }
+    }
+}
