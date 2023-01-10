0a0dd378d17216e6c04b8be0a63e1144bd767c5f
dianGrigorov <diangrigorov@gmail.com>
2023-01-10 13:23:08 +0200

    Create 06.EqualSums.cs

---
+++ 03.ArraysExercise/06.EqualSums.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            if (numbers.Length <= 1)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+               
+                int rightSum = 0;
+                int leftSum = 0;
+
+                for (int j = i +1; j < numbers.Length; j++)
+                {
+                    rightSum+= numbers[j];
+
+                }
+                
+                for (int k = i -1; k >= 0; k--)
+                {
+                    leftSum += numbers[k];
+                }
+               
+
+                if (rightSum == leftSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
