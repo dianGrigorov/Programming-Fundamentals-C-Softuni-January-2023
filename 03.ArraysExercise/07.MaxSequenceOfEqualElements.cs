179785e842d130ca04022f7ee7f43b2bc6388857
dianGrigorov <diangrigorov@gmail.com>
2023-01-10 16:45:28 +0200

    Create 07.MaxSequenceOfEqualElements.cs

---
+++ 03.ArraysExercise/07.MaxSequenceOfEqualElements.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int bestCnt = 0;
+            int bestIndex = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int bestSequence = 1;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        bestSequence++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (bestSequence > bestCnt)
+                {
+                    bestCnt = bestSequence;
+                    bestIndex = i;
+                }
+
+
+            }
+            for (int i = 0; i < bestCnt; i++)
+            {
+                Console.Write(numbers[bestIndex] + " ");
+            }
+        }
+    }
+}
