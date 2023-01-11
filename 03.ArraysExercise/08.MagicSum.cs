369464d864c5b9e65ea0fc57ca260c1c7dd45d80
dianGrigorov <diangrigorov@gmail.com>
2023-01-11 10:47:45 +0200

    Create 08.MagicSum.cs

---
+++ 03.ArraysExercise/08.MagicSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num.Length; i++)
+            {
+
+                for (int j = i +1; j < num.Length; j++)
+                {
+                    if (num[i] + num[j] == n)
+                    {
+                        Console.WriteLine(num[i] + " " + num[j]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
