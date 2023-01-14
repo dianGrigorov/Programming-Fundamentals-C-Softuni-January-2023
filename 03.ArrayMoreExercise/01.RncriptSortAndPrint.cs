13e28163edd299a580c3b3cf1f04afe84164e163
dianGrigorov <diangrigorov@gmail.com>
2023-01-14 13:57:35 +0200

    Update 01.RncriptSortAndPrint.cs

--- 03.ArrayMoreExercise/01.RncriptSortAndPrint.cs
+++ 03.ArrayMoreExercise/01.RncriptSortAndPrint.cs
@@ -1,35 +1,35 @@
 ﻿int n = int.Parse(Console.ReadLine());
-int[] colectSnm= new int[n];
+int[] colectSnm = new int[n];
 
 for (int i = 0; i < n; i++)
 {
-    string[] sequences = Console.ReadLine().Split();
+    string[] sequences = Console.ReadLine().Split("", StringSplitOptions.RemoveEmptyEntries);
 
-	foreach (var sequence in sequences)
-	{
-		
-		foreach (var letter in sequence)
-		{
-			char curCh = letter;
-			if (curCh == 'a' || curCh == 'A' ||
-				curCh == 'e' || curCh == 'E' ||
+    foreach (var sequence in sequences)
+    {
+
+        foreach (var letter in sequence)
+        {
+            char curCh = letter;
+            if (curCh == 'a' || curCh == 'A' ||
+                curCh == 'e' || curCh == 'E' ||
                 curCh == 'i' || curCh == 'I' ||
                 curCh == 'o' || curCh == 'O' ||
-				curCh == 'u' || curCh == 'U' )
-				
-			{
-				colectSnm[i] += curCh * sequence.Length ;
-			}
-			else
-			{
-				colectSnm[i] += curCh / sequence.Length;
-			}
+                curCh == 'u' || curCh == 'U')
+
+            {
+                colectSnm[i] += curCh * sequence.Length;
+            }
+            else
+            {
+                colectSnm[i] += curCh / sequence.Length;
+            }
         }
-	}
-	
+    }
+
 }
 Array.Sort(colectSnm);
 foreach (var colect in colectSnm)
 {
-	Console.WriteLine(string.Join(" ", colect));
+    Console.WriteLine(string.Join(" ", colect));
 }
