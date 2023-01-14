1a87ea3ea2f530e2ed2a77ef4919fd14bc782127
dianGrigorov <diangrigorov@gmail.com>
2023-01-14 13:48:43 +0200

    Create 01.RncriptSortAndPrint.cs

---
+++ 03.ArrayMoreExercise/01.RncriptSortAndPrint.cs
@@ -0,0 +1,35 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] colectSnm= new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] sequences = Console.ReadLine().Split();
+
+	foreach (var sequence in sequences)
+	{
+		
+		foreach (var letter in sequence)
+		{
+			char curCh = letter;
+			if (curCh == 'a' || curCh == 'A' ||
+				curCh == 'e' || curCh == 'E' ||
+                curCh == 'i' || curCh == 'I' ||
+                curCh == 'o' || curCh == 'O' ||
+				curCh == 'u' || curCh == 'U' )
+				
+			{
+				colectSnm[i] += curCh * sequence.Length ;
+			}
+			else
+			{
+				colectSnm[i] += curCh / sequence.Length;
+			}
+        }
+	}
+	
+}
+Array.Sort(colectSnm);
+foreach (var colect in colectSnm)
+{
+	Console.WriteLine(string.Join(" ", colect));
+}
