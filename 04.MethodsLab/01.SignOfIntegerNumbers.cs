9543e0a9227f5583e9c563ddfb836ec2fd9b126b
dianGrigorov <diangrigorov@gmail.com>
2023-01-15 15:06:35 +0200

    Create 01.SignOfIntegerNumbers.cs

---
+++ 04.MethodsLab/01.SignOfIntegerNumbers.cs
@@ -0,0 +1,19 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+CheckNumber(number);
+
+static void CheckNumber(int number)
+{
+    if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive. ");
+    }
+    else if (number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative. ");
+    }
+    else
+    {
+        Console.WriteLine($"The number {number} is zero. ");
+    }
+}
