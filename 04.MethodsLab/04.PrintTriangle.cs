47400eb7cf0cdb9ecc8cfd409e17ba5f4aeaadf0
dianGrigorov <diangrigorov@gmail.com>
2023-01-15 17:30:28 +0200

    Create 04.PrintTriangle.cs

---
+++ 04.MethodsLab/04.PrintTriangle.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    ColumnMethod(i); ;
+}
+for (int i = n -1; i >= 1; i--)
+{
+    ColumnMethod(i);
+}
+static void ColumnMethod(int i)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(j + " ");
+    }
+    Console.WriteLine();
+}
