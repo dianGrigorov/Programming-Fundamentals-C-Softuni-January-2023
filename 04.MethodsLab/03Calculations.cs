064b8f270f97ad194b7e7827cc6c3eecda984722
dianGrigorov <diangrigorov@gmail.com>
2023-01-15 15:06:42 +0200

    Create 03Calculations.cs

---
+++ 04.MethodsLab/03Calculations.cs
@@ -0,0 +1,41 @@
+﻿string command = Console.ReadLine();
+int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+
+if (command == "add")
+{
+    AddMethod(firstNum, secondNum);
+}
+else if (command == "multiply")
+{
+    MultiplyMethod(firstNum, secondNum);
+}
+else if (command == "subtract")
+{
+    SubstractMethod(firstNum, secondNum);
+}
+else if (command == "divide")
+{
+    DivideMethod(firstNum, secondNum);
+}
+
+static void AddMethod(int firstNum, int secondNum)
+{
+    int result = firstNum + secondNum;
+}
+
+static void MultiplyMethod(int firstNum, int secondNum)
+{
+    int result = firstNum * secondNum;
+    Console.WriteLine(result);
+}
+static void SubstractMethod(int firstNum, int secondNum)
+{
+    int result = firstNum - secondNum;
+    Console.WriteLine(result);
+}
+static void DivideMethod(int firstNum, int secondNum)
+{ 
+    int result = firstNum/ secondNum;
+    Console.WriteLine(result);
+}
