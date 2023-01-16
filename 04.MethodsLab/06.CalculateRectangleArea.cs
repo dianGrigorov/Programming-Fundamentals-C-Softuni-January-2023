743324a0da40a61215134aa70df65ede15adc1fd
dianGrigorov <diangrigorov@gmail.com>
2023-01-16 13:33:08 +0200

    Create 06.CalculateRectangleArea.cs

---
+++ 04.MethodsLab/06.CalculateRectangleArea.cs
@@ -0,0 +1,8 @@
+﻿double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+double area = CalculateRectangleArea(width, height);
+Console.WriteLine(area);
+static double CalculateRectangleArea(double width, double heigth)
+{
+    return width * heigth;
+}
