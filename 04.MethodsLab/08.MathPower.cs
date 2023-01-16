2f6c821a8def94311b89532697290ea8d8cf7097
dianGrigorov <diangrigorov@gmail.com>
2023-01-16 13:33:13 +0200

    Create 08.MathPower.cs

---
+++ 04.MethodsLab/08.MathPower.cs
@@ -0,0 +1,10 @@
+﻿double num = double.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+double result = MathPowerOfNum(num, power);
+Console.WriteLine(result);
+static double MathPowerOfNum(double num, int power)
+{
+    double result = Math.Pow(num, power);
+   
+    return result;
+}
