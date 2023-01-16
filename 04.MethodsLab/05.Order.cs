e03610e56ab0636348c48445d8bfa531bb91d2cd
dianGrigorov <diangrigorov@gmail.com>
2023-01-16 13:33:05 +0200

    Create 05.Order.cs

---
+++ 04.MethodsLab/05.Order.cs
@@ -0,0 +1,28 @@
+﻿
+string product = Console.ReadLine();
+int quantity = int.Parse(Console.ReadLine());
+
+CalculatePrice(product, quantity);
+static void CalculatePrice(string product, int quantity)
+{
+    if (product == "coffee")
+    {
+        decimal sum = quantity * 1.50m;
+        Console.WriteLine($"{sum:f2}");
+    }
+    else if (product == "water")
+    {
+        decimal sum = quantity * 1.00m;
+        Console.WriteLine($"{sum:f2}");
+    }
+    else if (product == "coke")
+    {
+        decimal sum = quantity * 1.40m;
+        Console.WriteLine($"{sum:f2}");
+    }
+    else if (product == "snacks")
+    {
+        decimal sum = quantity * 2.00m;
+        Console.WriteLine($"{sum:f2}");
+    }
+}
