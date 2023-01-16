dc16f781b78236dae97a39d0de210423ba239fa8
dianGrigorov <diangrigorov@gmail.com>
2023-01-16 13:33:10 +0200

    Create 07.RepeatingString.cs

---
+++ 04.MethodsLab/07.RepeatingString.cs
@@ -0,0 +1,14 @@
+﻿string input = Console.ReadLine();
+int n = int.Parse(Console.ReadLine());
+
+string result = RepetingString(input, n);
+Console.WriteLine(result);
+static string RepetingString(string input, int n)
+{
+	string result = string.Empty;
+	for (int i = 0; i < n; i++)
+	{
+		result += input;
+	}
+	return result;
+}
