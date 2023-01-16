ad472e755d872e1c6322e7cac17f78076f77249d
dianGrigorov <diangrigorov@gmail.com>
2023-01-16 16:10:38 +0200

    Create 11.MathOperation.cs

---
+++ 04.MethodsLab/11.MathOperation.cs
@@ -0,0 +1,28 @@
+﻿double n1 = double.Parse(Console.ReadLine());
+string operation = Console.ReadLine();
+double n2 = double.Parse(Console.ReadLine());
+
+double sum = MathOperation(operation, n1, n2);
+Console.WriteLine(sum);
+
+static double MathOperation(string operation, double n1, double n2)
+{
+	double sum = 0;
+	if (operation == "/")
+	{
+		sum = n1 / n2;
+	}
+	else if (operation == "*")
+	{
+		sum = n1 * n2;
+	}
+	else if (operation == "+")
+	{
+		sum = n1 + n2;
+	}
+	else if (operation == "-")
+	{
+		sum = n1 - n2;
+	}
+	return sum;
+}
