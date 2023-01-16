74c49d823551dca0c7f670c6c7a8719603cecaad
dianGrigorov <diangrigorov@gmail.com>
2023-01-16 16:10:41 +0200

    Create 10.MultiplyEvensByOdd.cs

---
+++ 04.MethodsLab/10.MultiplyEvensByOdd.cs
@@ -0,0 +1,36 @@
+﻿string input = Console.ReadLine();
+int absNum = Math.Abs(int.Parse(input));
+
+int sumEven = GetSumOfEvenSum(absNum.ToString());
+int sumOdd = GetSumOfOddSum(absNum.ToString());
+
+int multiply = sumEven * sumOdd;
+Console.WriteLine(multiply);
+
+static int GetSumOfOddSum(string input)
+{
+    int sum = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        int curNum = int.Parse(input[i].ToString());
+        if (curNum %2 != 0)
+        {
+            sum += curNum;
+        }
+    }
+    return sum;
+}
+
+static int GetSumOfEvenSum(string input)
+{
+    int sum = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        int curNum = int.Parse(input[i].ToString());
+        if (curNum % 2 == 0)
+        {
+            sum += curNum;
+        }
+    }
+    return sum;
+}
