da144466d62450ab3ce70f8605ed6a10a13fefac
dianGrigorov <diangrigorov@gmail.com>
2023-01-17 23:16:44 +0200

    Update 02.PascalTriangle.cs

--- 03.ArrayMoreExercise/02.PascalTriangle.cs
+++ 03.ArrayMoreExercise/02.PascalTriangle.cs
@@ -1,19 +1,20 @@
 ﻿int n = int.Parse(Console.ReadLine());
 
 int[] arr = null;
-int[] previosArr= null;
+int[] previosArr = null;
 
 for (int i = 1; i <= n; i++)
 {
     arr = new int[i];
     arr[0] = 1;                    //The firs element is allways 1
-    arr[arr.Length-1] = 1;         // The last element is allways1
+    arr[arr.Length - 1] = 1;         // The last element is allways1
 
-    for (int j = 1; j < arr.Length -1; j++)     // Calculate column
+    for (int j = 1; j < arr.Length - 1; j++)
     {
-        arr[j] = previosArr[previosArr.Length - 1] + previosArr[j];
+        arr[j] = previosArr[j - 1] + previosArr[j];
     }
-    previosArr = arr;
 
-    Console.WriteLine(string.Join(' ', arr));
+    previosArr = arr;             //Save previos row
+
+    Console.WriteLine(string.Join(" ", arr));
 }
