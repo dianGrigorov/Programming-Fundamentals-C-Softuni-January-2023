f60467f93d7b91d9dd767c75f7b9692d68acfb27
dianGrigorov <diangrigorov@gmail.com>
2023-01-17 15:40:55 +0200

    Add tow more task

---
+++ 04.MethodsExercise/01.SmallestOfThreeNumbers.cs
@@ -0,0 +1,14 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+ChecMinNumber(a, b, c);
+static void ChecMinNumber(int a, int b, int c)
+{
+    int min = Math.Min(a, b);
+    if (min > c)
+    {
+        min = c;
+    }
+    Console.WriteLine(min);
+}
---
+++ 04.MethodsExercise/02.VowelsCount.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+int cnt = CountVowelsLeter(input);
+Console.WriteLine(cnt);
+static int CountVowelsLeter(string input)
+{
+    char[] chars = new char[] { 'a', 'e', 'i', 'o', 'u', 'y'};
+    int cntChar = 0;
+    
+    
+        foreach (var item in input.ToLower())
+        {
+            if (chars.Contains(item))
+            {
+            cntChar++;
+            }
+        }
+    
+    return cntChar;
+}
