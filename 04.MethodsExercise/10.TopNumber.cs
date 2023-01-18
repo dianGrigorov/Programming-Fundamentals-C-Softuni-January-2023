e4fd09f1298439aad19e0bacf583ade446f25ea9
dianGrigorov <diangrigorov@gmail.com>
2023-01-18 17:49:21 +0200

    Create 10.TopNumber.cs

---
+++ 04.MethodsExercise/10.TopNumber.cs
@@ -0,0 +1,28 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+IsDivisibleByEight(number);
+
+static void IsDivisibleByEight(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        int num = i;
+        int curentNum = 0;
+        bool isDiferentFromZero = false;
+
+        while (num > 0)
+        {
+            curentNum += num % 10;
+            if (num % 2 != 0)
+            {
+                isDiferentFromZero = true;
+            }
+            
+            num /= 10;
+        }
+        if (curentNum % 8 == 0 && isDiferentFromZero == true)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
