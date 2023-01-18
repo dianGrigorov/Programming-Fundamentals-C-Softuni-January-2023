f0197453f1f7bacdf9cbe5858c9fabebb1576853
dianGrigorov <diangrigorov@gmail.com>
2023-01-18 17:49:24 +0200

    Create 09.PalindromInteger.cs

---
+++ 04.MethodsExercise/09.PalindromInteger.cs
@@ -0,0 +1,28 @@
+﻿string input;
+while ((input = Console.ReadLine()) != "END")
+{
+    if (IsPalindromeOrNot(input))
+    {
+        Console.WriteLine("true");
+    }
+    else
+    {
+        Console.WriteLine("false");
+    }
+}
+
+static bool IsPalindromeOrNot(string input)
+{
+    string reverceString = string.Empty;
+
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        reverceString += input[i];
+    }
+
+    if (input == reverceString)
+    {
+        return true;
+    }
+    return false;
+}
