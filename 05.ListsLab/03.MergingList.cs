f330a328ab7486e05f9f15b1bdffab7c0fcabdf0
dianGrigorov <diangrigorov@gmail.com>
2023-01-20 14:01:22 +0200

    Create 03.MergingList.cs

---
+++ 05.ListsLab/03.MergingList.cs
@@ -0,0 +1,34 @@
+﻿List<int> firstNum = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+
+List<int> secondNum = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+List<int> num = new List<int>();
+
+int n = Math.Min(firstNum.Count, secondNum.Count);
+for (int i = 0; i < n; i++)
+{
+    num.Add(firstNum[i]);
+    num.Add(secondNum[i]);
+}
+if (firstNum.Count > secondNum.Count)
+{
+    for (int i = n; i < firstNum.Count; i++)
+    {
+        num.Add(firstNum[i]);
+    }
+}
+if (secondNum.Count > firstNum.Count)
+{
+    for (int i = n; i < secondNum.Count; i++)
+    {
+        num.Add(secondNum[i]);
+    }
+}
+
+Console.WriteLine(string.Join(" ", num));
