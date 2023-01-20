d3346a5e229082def9b38da299d9c91987236145
dianGrigorov <diangrigorov@gmail.com>
2023-01-20 14:01:27 +0200

    Create 05.RemoveNegativeAndReverse.cs

---
+++ 05.ListsLab/05.RemoveNegativeAndReverse.cs
@@ -0,0 +1,14 @@
+﻿List<int> num = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+
+num.RemoveAll(x => x < 0);
+num.Reverse();
+
+if (num.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+
+Console.WriteLine(string.Join(" ", num));
