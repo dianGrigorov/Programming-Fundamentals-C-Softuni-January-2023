ef4a39ec3c123fec415911939c9f4d73bf867bf7
dianGrigorov <diangrigorov@gmail.com>
2023-01-20 14:01:24 +0200

    Create 04.ListOfProducts.cs

---
+++ 05.ListsLab/04.ListOfProducts.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<string> product = new List<string>();
+for (int i = 0; i < n; i++)
+{
+    string curentProduct = Console.ReadLine();
+    product.Add(curentProduct);
+    
+}
+product.Sort();
+
+for (int i = 0; i < product.Count; i++)
+{
+
+    Console.WriteLine($"{i + 1}.{product[i]}");
+}
+
