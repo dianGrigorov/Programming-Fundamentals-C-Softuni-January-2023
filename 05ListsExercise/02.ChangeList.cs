f9916b5701a124af839029f22f555339b5d7e50b
dianGrigorov <diangrigorov@gmail.com>
2023-01-21 02:15:06 +0200

    Create 02.ChangeList.cs

---
+++ 05ListsExercise/02.ChangeList.cs
@@ -0,0 +1,24 @@
+﻿List<int> num = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+string input;
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] cmd = input.Split();
+    string currcmd = cmd[0];
+    if (currcmd == "Delete")
+    {
+        int number = int.Parse(cmd[1]);
+        num.RemoveAll(x => x == number);
+    }
+    else if (currcmd == "Insert")
+    {
+        int number = int.Parse(cmd[1]);
+        int index = int.Parse(cmd[2]);
+
+        num.Insert(index, number);
+    }
+}
+
+Console.WriteLine(string.Join(" ", num));
