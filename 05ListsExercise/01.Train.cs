b0456ace577eb1d7a3acc43a81a84ca9e9d24e97
dianGrigorov <diangrigorov@gmail.com>
2023-01-21 02:15:03 +0200

    Create 01.Train.cs

---
+++ 05ListsExercise/01.Train.cs
@@ -0,0 +1,31 @@
+﻿List<int> vagons = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+
+int maxCapacity = int.Parse(Console.ReadLine());
+string input;
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] cmd = input.Split(" ");
+    string currCmd = cmd[0];
+    if (currCmd == "Add")
+    {
+        int passanger = int.Parse(cmd[1]);
+        vagons.Add(passanger);
+    }
+    else
+    {
+        int passangers = int.Parse(cmd[0]);
+        for (int i = 0; i < vagons.Count; i++)
+        {
+            if (vagons[i] + passangers <= maxCapacity)
+            {
+                vagons[i] += passangers;
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", vagons));
