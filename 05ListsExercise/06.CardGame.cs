eb2fe954ab5f06f8c6e940ab9e71f8fe0767cea0
dianGrigorov <diangrigorov@gmail.com>
2023-01-24 13:08:17 +0200

    Create 06.CardGame.cs

---
+++ 05ListsExercise/06.CardGame.cs
@@ -0,0 +1,40 @@
+﻿List<int> firstPlayer = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+
+List<int> secondPlayer = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+while (firstPlayer.Count > 0 && secondPlayer.Count > 0)
+{
+    
+    if (firstPlayer[0] > secondPlayer[0])
+    {
+        firstPlayer.Add(secondPlayer[0]);
+        firstPlayer.Add(firstPlayer[0]);
+    }
+    else if (secondPlayer[0] > firstPlayer[0])
+    {
+
+        secondPlayer.Add(firstPlayer[0]);
+        secondPlayer.Add(secondPlayer[0]);
+    }
+    
+
+    firstPlayer.RemoveAt(0);
+    secondPlayer.RemoveAt(0);
+
+    if (firstPlayer.Count == 0)
+    {
+        Console.WriteLine($"Second player wins! Sum: {secondPlayer.Sum()}");
+        break;
+    }
+    if (secondPlayer.Count == 0)
+    {
+        Console.WriteLine($"First player wins! Sum: {firstPlayer.Sum()}");
+        break;
+    }
+}
