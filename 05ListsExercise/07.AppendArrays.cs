032c1f5ef28ed9832b2416d5767eae78713f91cf
dianGrigorov <diangrigorov@gmail.com>
2023-01-24 14:17:54 +0200

    Create 07.AppendArrays.cs

---
+++ 05ListsExercise/07.AppendArrays.cs
@@ -0,0 +1,15 @@
+﻿List<string> input = Console.ReadLine()
+        .Split("|", StringSplitOptions.RemoveEmptyEntries)
+        .ToList();
+
+input.Reverse();
+List<int> num = new List<int>();
+
+foreach (var number in input)
+{
+    num.AddRange(number.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToList());
+}
+
+Console.WriteLine(string.Join(" ", num));
