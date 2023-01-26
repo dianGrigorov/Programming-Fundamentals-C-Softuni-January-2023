deefd7315a532f9656e9a32e15572b9fd1d87608
dianGrigorov <diangrigorov@gmail.com>
2023-01-26 16:36:54 +0200

    Create 01,ComputerStor.cs

---
+++ Fund-MidExamPractis/01.MidEzam/01,ComputerStor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ComputerStor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string command;
+            decimal sum = 0;
+            while ((command = Console.ReadLine()) != "special")
+            {
+                if (command == "regular")
+                {
+                    break;
+                }
+                decimal partPrice = decimal.Parse(command);
+                if (partPrice < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+
+                }
+                else
+                {
+                    sum += partPrice;
+                }
+
+            }
+            decimal taxesPrice = sum * 0.2m;
+            decimal totalSum = sum + taxesPrice;
+
+            if (command == "special")
+            {
+                totalSum *= 0.9m;
+            }
+            if (sum <= 0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {sum:f2}$");
+                Console.WriteLine($"Taxes: {taxesPrice:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {totalSum:f2}$");
+
+            }
+        }
+    }
+}
