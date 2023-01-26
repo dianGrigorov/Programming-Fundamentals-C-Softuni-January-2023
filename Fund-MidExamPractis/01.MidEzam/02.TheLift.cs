d6c923f3f1266e71c1312e48adf9d69d98862e71
dianGrigorov <diangrigorov@gmail.com>
2023-01-26 16:36:57 +0200

    Create 02.TheLift.cs

---
+++ Fund-MidExamPractis/01.MidEzam/02.TheLift.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int[] lift = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            // 0 0 0 0
+            for (int i = 0; i < lift.Length; i++)
+            {
+                if (lift[i] < 4)
+                {
+
+                    for (int j = lift[i]; j < 4; j++)
+                    {
+                        lift[i]++;
+                        people--;
+                        if (people == 0)
+                        {
+                            
+                            break;
+                        }
+                    }
+                    if (people == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (people > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {people} people in a queue!");
+                Console.WriteLine(string.Join(" ", lift));
+            }
+            else if (lift.All(x => x == 4) && people == 0)
+            {
+                Console.WriteLine(string.Join(" ", lift));
+            }
+            else
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", lift));
+            }
+        }
+    }
+}
