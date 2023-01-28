9992802b52145ace66e654bcbe4c0ed773731e64
dianGrigorov <diangrigorov@gmail.com>
2023-01-28 08:18:24 +0200

    Create 01.SoftUniReception.cs

---
+++ Fund-MidExamPractis/02.MidExam/01.SoftUniReception.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01SoftUniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstrEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int secondEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int thirdEmployeeEfficiency = int.Parse(Console.ReadLine());
+
+            int studentsQuestions = int.Parse(Console.ReadLine());
+
+            int totalHours = 0;
+
+            int questionsPerHour = firstrEmployeeEfficiency + secondEmployeeEfficiency + thirdEmployeeEfficiency;
+
+            while (studentsQuestions > 0)
+            {
+                totalHours++;
+                if (totalHours % 4 == 0)
+                {
+                    if (studentsQuestions <= 0)
+                    {
+                        break;
+                    }
+                    continue;
+                }
+                studentsQuestions -= questionsPerHour;
+            }
+
+            Console.WriteLine($"Time needed: {totalHours}h.");
+        }
+    }
+}
