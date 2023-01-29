dcf55cc0e781964c7cfe8c9dfc295a145dd24160
dianGrigorov <diangrigorov@gmail.com>
2023-01-30 01:55:11 +0200

    Create 01.RandomizeWord.cs

---
+++ 06.ObjectsAndClasses/01.RandomizeWord.cs
@@ -0,0 +1,17 @@
+﻿string[] words = Console.ReadLine().Split(" ");
+
+Random word = new Random();
+
+for (int i = 0; i < words.Length; i++)
+{
+    int rndIndex = word.Next(0, words.Length);
+    // 1 2 3 4 5 6
+    string currentWord = words[i];
+    words[i] = words[rndIndex];
+    words[rndIndex] = currentWord;
+    
+
+    
+    
+}
+Console.WriteLine(string.Join(Environment.NewLine, words));
