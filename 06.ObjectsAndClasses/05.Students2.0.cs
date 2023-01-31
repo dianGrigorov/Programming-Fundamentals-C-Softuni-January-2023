b63b32ea6f7a89af7a2f107eafb3972527ad9450
dianGrigorov <diangrigorov@gmail.com>
2023-01-31 17:01:47 +0200

    Create 05.Students2.0.cs

---
+++ 06.ObjectsAndClasses/05.Students2.0.cs
@@ -0,0 +1,57 @@
+﻿List<Students> students = new List<Students>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] studentInput = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string firstName = studentInput[0];
+    string lastName = studentInput[1];
+    int age = int.Parse(studentInput[2]);
+    string town = studentInput[3];
+
+    Students currStudent = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+    if (currStudent == null)
+    {
+        students.Add(new Students(firstName, lastName, age, town));
+    }
+    else
+    {
+        currStudent.FirstName = firstName;
+        currStudent.LastName = lastName;
+        currStudent.Age = age;
+        currStudent.Town = town;
+    }
+
+
+}
+
+string filterCity = Console.ReadLine();
+
+foreach (Students student in students)
+{
+    if (student.Town == filterCity)
+    {
+        Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+    }
+}
+public class Students
+{
+    public Students(string firstName, string lastName, int age, string town)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Town = town;
+    }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+
+    public string Town { get; set; }
+}
