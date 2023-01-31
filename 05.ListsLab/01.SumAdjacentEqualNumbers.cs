766e39cb3b4e79db20c4d2fe16bc51ce2f282c17
dianGrigorov <diangrigorov@gmail.com>
2023-01-31 17:07:42 +0200

    Create 01.SumAdjacentEqualNumbers.cs

---
+++ 05.ListsLab/01.SumAdjacentEqualNumbers.cs
@@ -0,0 +1,15 @@
+﻿List<int> num = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList(); ;
+List<int> gaus = new List<int>();
+for (int i = 0; i < num.Count / 2; i++)
+{
+    gaus.Add(num[i] + num[num.Count - i -1]);
+ 
+}
+if (num.Count % 2 != 0)
+{
+    gaus.Add(num[num.Count / 2]);
+}
+Console.WriteLine(string.Join(" ",gaus));
