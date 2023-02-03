ecd9f353763cf1807d860a74ed31f5542840269e
dianGrigorov <diangrigorov@gmail.com>
2023-02-03 13:52:56 +0200

    Create 02.ShopingList.cs

---
+++ Fund-MidExamPractis/04.MidExam/02.ShopingList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ShopingList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<string> shopingList = Console.ReadLine().Split("!", StringSplitOptions.RemoveEmptyEntries).ToList();
+            string input;
+
+            while ((input = Console.ReadLine()) != "Go Shopping!")
+            {
+                string[] commandArguments = input.Split(" ");
+
+                string command = commandArguments[0];
+                UrgentCommand(shopingList, commandArguments, command);
+
+                UnnecesseryCommand(shopingList, commandArguments, command);
+
+                CorrectCommand(shopingList, commandArguments, command);
+
+                RearrangeCommand(shopingList, commandArguments, command);
+            }
+
+            Console.WriteLine(string.Join(", ", shopingList));
+        }
+
+        private static void RearrangeCommand(List<string> shopingList, string[] commandArguments, string command)
+        {
+            if (command == "Rearrange")
+            {
+                string item = commandArguments[1];
+
+                for (int i = 0; i < shopingList.Count; i++)
+                {
+                    if (item == shopingList[i])
+                    {
+                        shopingList.RemoveAt(i);
+                        shopingList.Add(item);
+                    }
+                }
+
+            }
+        }
+
+        private static void CorrectCommand(List<string> shopingList, string[] commandArguments, string command)
+        {
+            if (command == "Correct")
+            {
+                string oldItem = commandArguments[1];
+                string newItem = commandArguments[2];
+
+                for (int i = 0; i < shopingList.Count; i++)
+                {
+                    if (oldItem == shopingList[i])
+                    {
+                        shopingList[i] = newItem;
+                    }
+                   
+                }
+
+            }
+        }
+
+        private static void UnnecesseryCommand(List<string> shopingList, string[] commandArguments, string command)
+        {
+            if (command == "Unnecessery")
+            {
+                string item = commandArguments[1];
+               
+                    if (shopingList.Contains(item))
+                    {
+                        shopingList.Remove(item);
+                    }
+                   
+            }
+        }
+
+        private static void UrgentCommand(List<string> shopingList, string[] commandArguments, string command)
+        {
+            if (command == "Urgent")
+            {
+                string item = commandArguments[1];
+         
+                
+                    if (shopingList.Contains(item))
+                    {
+                        
+                    }
+                    else
+                    {
+                        shopingList.Insert(0, item);
+                    }
+                
+            }
+        }
+    }
+}
