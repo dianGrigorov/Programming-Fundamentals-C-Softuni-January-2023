2665ea29382fbc3e5f5a9d331da3ee549522c370
dianGrigorov <diangrigorov@gmail.com>
2023-02-04 18:49:07 +0200

    Create 03.WordSynonyms.cs

---
+++ 07.AssociativeArraysLab/03.WordSynonyms.cs
@@ -0,0 +1,22 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < n; i++)
+{
+    string word = Console.ReadLine();
+    string synonyms = Console.ReadLine();
+
+    if (!words.ContainsKey(word))
+    {
+        words.Add(word, new List<string>());
+    }
+
+    words[word].Add(synonyms);
+}
+
+foreach (var word in words)
+{
+    Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+}
