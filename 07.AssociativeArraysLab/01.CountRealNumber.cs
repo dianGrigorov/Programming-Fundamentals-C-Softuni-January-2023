9cfe98c811e8dea27d6ab06d83c49b56288a7464
dianGrigorov <diangrigorov@gmail.com>
2023-02-04 18:00:28 +0200

    Create 01.CountRealNumber.cs

---
+++ 07.AssociativeArraysLab/01.CountRealNumber.cs
@@ -0,0 +1,22 @@
+﻿
+List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+SortedDictionary<int, int> result = new SortedDictionary<int, int>();
+
+foreach (var number in numbers)
+{
+    if (!result.ContainsKey(number))
+    {
+        result.Add(number, 0);
+    }
+
+    result[number]++;
+}
+
+foreach(var number in result)
+{
+    Console.WriteLine($"{number.Key} -> {number.Value}");
+}
