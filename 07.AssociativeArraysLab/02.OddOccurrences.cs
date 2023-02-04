71dea490263bb3e77ba076b6082138d1836bd3dd
dianGrigorov <diangrigorov@gmail.com>
2023-02-04 18:24:54 +0200

    Create 02.OddOccurrences.cs

---
+++ 07.AssociativeArraysLab/02.OddOccurrences.cs
@@ -0,0 +1,24 @@
+﻿
+string[] words = Console.ReadLine().Split();
+
+Dictionary<string, int> counts = new Dictionary<string, int>();
+
+foreach (string word in words)
+{
+    string wordInLowerCase = word.ToLower();
+
+    if (!counts.ContainsKey(wordInLowerCase))
+    {
+        counts.Add(wordInLowerCase, 0);
+    }
+    counts[wordInLowerCase]++;
+}
+
+foreach (var count in counts)
+{
+    if (count.Value % 2 != 0)
+    {
+        Console.Write($"{count.Key}" + " ");
+    }
+}
+
