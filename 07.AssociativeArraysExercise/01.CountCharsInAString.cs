4a12e4446fd96a79d94cbf1ae676660791fc01a8
dianGrigorov <diangrigorov@gmail.com>
2023-02-05 18:04:46 +0200

    Create 01.CountCharsInAString.cs

---
+++ 07.AssociativeArraysExercise/01.CountCharsInAString.cs
@@ -0,0 +1,21 @@
+﻿string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Dictionary<char, int> occurrences = new Dictionary<char, int>();
+
+foreach(string word in words)
+{
+    string currentWord = word;
+
+	foreach (char item in currentWord)
+	{
+		if (!occurrences.ContainsKey(item))
+		{
+			occurrences.Add(item, 0);
+		}
+		occurrences[item]++;
+	}
+}
+foreach(var item in occurrences)
+{
+	Console.WriteLine($"{item.Key} -> {item.Value}");
+}
