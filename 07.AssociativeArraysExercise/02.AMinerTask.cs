c363956bb2225e9d338ddb2116c2d9e44d539a8c
dianGrigorov <diangrigorov@gmail.com>
2023-02-05 18:04:35 +0200

    Create 02.AMinerTask.cs

---
+++ 07.AssociativeArraysExercise/02.AMinerTask.cs
@@ -0,0 +1,20 @@
+﻿
+Dictionary<string, int> resources = new Dictionary<string, int>();
+
+string input;
+while ((input = Console.ReadLine()) != "stop")
+{
+    string resource = input;
+    int quantity = int.Parse(Console.ReadLine());
+
+    if (!resources.ContainsKey(resource))
+    {
+        resources.Add(resource, 0);
+    }
+    resources[resource] += quantity;
+}
+
+foreach(var resource in resources)
+{
+    Console.WriteLine($"{resource.Key} -> {resource.Value}");
+}
