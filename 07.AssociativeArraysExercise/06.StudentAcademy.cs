e233c4982e44526c5515d3fb27912b6d306c3f10
dianGrigorov <diangrigorov@gmail.com>
2023-02-06 12:59:08 +0200

    Create 06.StudentAcademy.cs

---
+++ 07.AssociativeArraysExercise/06.StudentAcademy.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Xml.Linq;
+
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> studentsInfo = new Dictionary<string, List<decimal>>();
+
+for(int i = 0;i < n; i++)
+{
+    string studentName = Console.ReadLine();
+    decimal grade = decimal.Parse(Console.ReadLine());
+
+    if (!studentsInfo.ContainsKey(studentName))
+    {
+        studentsInfo.Add(studentName, new List<decimal>());
+    }
+
+    studentsInfo[studentName].Add(grade);
+}
+
+
+foreach (var item in studentsInfo)
+{
+    
+    if (item.Value.Average(x => x) >= 4.5m)
+    {
+        Console.WriteLine($"{item.Key} –> {item.Value.Average():f2}");
+    }
+}
