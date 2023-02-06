d6620a8b4e9d4f2a4148fc074b83affb3c6feef6
dianGrigorov <diangrigorov@gmail.com>
2023-02-06 12:17:50 +0200

    Create 05.Courses.cs

---
+++ 07.AssociativeArraysExercise/05.Courses.cs
@@ -0,0 +1,29 @@
+﻿
+Dictionary<string, List<string>> courseInfo = new Dictionary<string, List<string>>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] inputArg = input.Split(" : ");
+
+    string courseName = inputArg[0];
+    string studentName = inputArg[1];
+
+    if (!courseInfo.ContainsKey(courseName))
+    {
+        courseInfo.Add(courseName, new List<string>());
+    }
+    courseInfo[courseName].Add(studentName);
+
+}
+
+foreach(var courseName in courseInfo)
+{
+    Console.WriteLine($"{courseName.Key}: {courseName.Value.Count}");
+
+    foreach (var studentName in courseName.Value)
+    {
+        Console.WriteLine($"-- {studentName}");
+    }
+}
