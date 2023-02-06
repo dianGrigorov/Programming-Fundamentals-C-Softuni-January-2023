ab0b1882fe300a1ae1b9fb010bb08e38ba06ad34
dianGrigorov <diangrigorov@gmail.com>
2023-02-06 13:26:11 +0200

    Create 07.CompanyUsers.cs

---
+++ 07.AssociativeArraysExercise/07.CompanyUsers.cs
@@ -0,0 +1,34 @@
+﻿
+Dictionary<string, List<string>> keyValuePairs= new Dictionary<string, List<string>>();
+string input;
+
+while ((input = Console.ReadLine()) != "End")
+{
+    string[] companyAndEmployees = input.Split("->", StringSplitOptions.RemoveEmptyEntries);
+    string company = companyAndEmployees[0];
+    string employeID = companyAndEmployees[1];
+
+    if (!keyValuePairs.ContainsKey(company))
+    {
+        keyValuePairs.Add(company, new List<string>());
+    }
+    if (keyValuePairs[company].Contains(employeID))
+    {
+        continue;
+    }
+    else
+    {
+        keyValuePairs[company].Add(employeID);
+    }
+
+}
+
+foreach (var item in keyValuePairs)
+{
+    Console.WriteLine($"{item.Key}");
+
+    foreach (var ID in item.Value)
+    {
+        Console.WriteLine($"--{ID}");
+    }
+}
