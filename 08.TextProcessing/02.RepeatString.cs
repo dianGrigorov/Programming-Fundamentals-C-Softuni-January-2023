7b4d142e9e76077bc84db4e95cfdf13c47a0280c
dianGrigorov <diangrigorov@gmail.com>
2023-02-08 12:53:59 +0200

    Create 02.RepeatString.cs

---
+++ 08.TextProcessing/02.RepeatString.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Text;
+
+string[] word = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+StringBuilder result = new StringBuilder();
+
+foreach (var w in word)
+{
+    int count = w.Length;
+	for (int i = 0; i < count; i++)
+	{
+		result = result.Append(w);
+	}
+}
+Console.WriteLine(result);
