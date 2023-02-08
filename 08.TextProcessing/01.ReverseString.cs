c72bf30045b3a34d9ed5eee92f9d0e01d5b8085c
dianGrigorov <diangrigorov@gmail.com>
2023-02-08 12:53:51 +0200

    Create 01.ReverseString.cs

---
+++ 08.TextProcessing/01.ReverseString.cs
@@ -0,0 +1,13 @@
+﻿
+string command;
+
+while ((command = Console.ReadLine()) != "end")
+{
+    string reverseWord = string.Empty;
+
+    for (int i = command.Length - 1; i >= 0; i--)
+    {
+        reverseWord += command[i];
+    }
+    Console.WriteLine($"{command} = {reverseWord}");
+}
