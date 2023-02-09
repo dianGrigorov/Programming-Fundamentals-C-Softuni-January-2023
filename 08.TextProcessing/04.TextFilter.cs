6e7b2cafcdaf9288d33bbc316b7a2a06c458b213
dianGrigorov <diangrigorov@gmail.com>
2023-02-09 13:08:10 +0200

    Create 04.TextFilter.cs

---
+++ 08.TextProcessing/04.TextFilter.cs
@@ -0,0 +1,12 @@
+﻿
+
+string[] bannedWords = Console.ReadLine().Split(", ");
+
+string text = Console.ReadLine();
+
+foreach(string currWord in bannedWords)
+{
+    text = text.Replace(currWord, new string('*', currWord.Length));
+}
+
+Console.WriteLine(text);
