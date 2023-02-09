d7cefe4ff6f9a6016958d71fd66bb967d17ddf81
dianGrigorov <diangrigorov@gmail.com>
2023-02-09 18:33:20 +0200

    Create 02.CharacterMultiplier.cs

---
+++ 08.TextProcessingExercise/02.CharacterMultiplier.cs
@@ -0,0 +1,36 @@
+﻿
+
+string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+MultiplierString(input);
+
+static void MultiplierString(string[] input)
+{ 
+    string firstStr = input[0];
+    string secondStr = input[1];
+	int sum = 0;
+	if (firstStr.Length < secondStr.Length)
+	{
+		for (int i = 0; i < firstStr.Length; i++)
+		{
+			sum += firstStr[i] * secondStr[i];
+		}
+		for (int i = firstStr.Length; i < secondStr.Length; i++)
+		{
+			sum += secondStr[i];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < secondStr.Length; i++)
+		{
+            sum += firstStr[i] * secondStr[i];
+        }
+
+		for (int i = secondStr.Length; i < firstStr.Length; i++)
+		{
+			sum += firstStr[i];
+		}
+	}
+	Console.WriteLine(sum);
+}
