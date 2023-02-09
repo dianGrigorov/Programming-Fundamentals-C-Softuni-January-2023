9f852069c3ed706aaa6e0974f1bcdaec616c3bd1
dianGrigorov <diangrigorov@gmail.com>
2023-02-09 18:33:18 +0200

    Create 01.ValidUsername.cs

---
+++ 08.TextProcessingExercise/01.ValidUsername.cs
@@ -0,0 +1,29 @@
+﻿
+
+string[] userName = Console.ReadLine().Split(", ");
+
+string validUserName = string.Empty;
+
+foreach (var name in userName)
+{
+    bool notContains = false;
+
+    foreach (var ch in name)
+    {
+        if (Char.IsLetterOrDigit(ch) || ch == '-' || ch =='_')
+        {
+            notContains = true;
+           
+        }
+        else
+        {
+            notContains = false;
+            break;
+        }
+        
+    }
+    if (notContains && name.Length >= 3 && name.Length <= 16)
+    {
+        Console.WriteLine(name);
+    }
+}
