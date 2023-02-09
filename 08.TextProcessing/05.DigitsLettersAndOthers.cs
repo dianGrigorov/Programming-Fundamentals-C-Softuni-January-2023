e45830065d9d7cd848b35d248e751bf5d967d801
dianGrigorov <diangrigorov@gmail.com>
2023-02-09 13:08:12 +0200

    Create 05.DigitsLettersAndOthers.cs

---
+++ 08.TextProcessing/05.DigitsLettersAndOthers.cs
@@ -0,0 +1,27 @@
+﻿
+
+string input = Console.ReadLine();
+
+foreach (char ch in input)
+{
+	if (Char.IsDigit(ch))
+	{
+		Console.Write(ch);
+	}
+}
+Console.WriteLine();
+foreach(char ch in input)
+{
+	if (Char.IsLetter(ch))
+	{
+		Console.Write(ch);
+	}
+}
+Console.WriteLine();
+foreach (char ch in input)
+{
+    if (!Char.IsLetterOrDigit(ch))
+    {
+        Console.Write(ch);
+    }
+}
