b54f3a7f85f82edb03d768de4d7a98415c0d5b61
dianGrigorov <diangrigorov@gmail.com>
2023-02-09 13:08:07 +0200

    Create 03.Substring.cs

---
+++ 08.TextProcessing/03.Substring.cs
@@ -0,0 +1,13 @@
+﻿
+string key = Console.ReadLine();
+string str = Console.ReadLine();
+
+
+while (str.Contains(key))
+{
+    int index = str.IndexOf(key);
+  
+    str = str.Remove(index, key.Length);
+}
+
+Console.WriteLine(str);
