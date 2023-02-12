788e59ea34d057f7c34a01997f93e0a8ed23335a
dianGrigorov <diangrigorov@gmail.com>
2023-02-12 20:32:46 +0200

    Create 04.CaesarCipher.cs

---
+++ 08.TextProcessingExercise/04.CaesarCipher.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text;
+
+string input = Console.ReadLine();
+
+char[] result = input.ToCharArray();
+
+for (int i = 0; i < input.Length; i++)
+{
+    result[i] = (char)(input[i] + 3);
+    
+}
+
+Console.WriteLine(string.Join("", result));
