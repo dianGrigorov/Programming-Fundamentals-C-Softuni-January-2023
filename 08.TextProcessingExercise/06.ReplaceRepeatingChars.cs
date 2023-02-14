63939185615abfcd36d1dc132bbbbc82f9419101
dianGrigorov <diangrigorov@gmail.com>
2023-02-14 05:38:03 +0200

    Create 06.ReplaceRepeatingChars.cs

---
+++ 08.TextProcessingExercise/06.ReplaceRepeatingChars.cs
@@ -0,0 +1,16 @@
+﻿
+string input = Console.ReadLine();
+
+char currChar = input[0];
+
+Console.Write(currChar);
+
+for (int i = 1; i < input.Length; i++)
+{
+	if (currChar != input[i])
+	{
+		currChar = input[i];
+
+		Console.Write(currChar);
+	}
+}
