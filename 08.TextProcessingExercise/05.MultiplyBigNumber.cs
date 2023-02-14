d5ca2282303ab0a9793ff7556610a2796e5f62e4
dianGrigorov <diangrigorov@gmail.com>
2023-02-14 05:30:26 +0200

    Create 05.MultiplyBigNumber.cs

---
+++ 08.TextProcessingExercise/05.MultiplyBigNumber.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.Text;
+
+string bigNum = Console.ReadLine();
+int multiply = int.Parse(Console.ReadLine());
+
+var finalResult = new StringBuilder();
+
+int reminder = 0;
+
+for (int i = bigNum.Length - 1; i >= 0; i--)
+{
+    int currDig = int.Parse(bigNum[i].ToString());
+    int product = currDig * multiply + reminder;
+    int result = product % 10;
+    reminder = product / 10;
+    finalResult.Insert(0, result);
+
+}
+if (reminder > 0)
+{
+    finalResult.Insert(0, reminder);
+}
+Console.WriteLine(finalResult.ToString());
