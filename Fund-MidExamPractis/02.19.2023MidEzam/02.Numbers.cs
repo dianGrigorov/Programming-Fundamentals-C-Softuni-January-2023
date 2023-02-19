1ecbd88e811e91eac6430f2e647ec53806d8ac35
dianGrigorov <diangrigorov@gmail.com>
2023-02-19 13:21:15 +0200

    Create 02.Numbers.cs

---
+++ Fund-MidExamPractis/02.19.2023MidEzam/02.Numbers.cs
@@ -0,0 +1,45 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string command;
+
+while ((command = Console.ReadLine()) != "Finish")
+{
+    string[] commArg = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string currCommand = commArg[0];
+    int value = int.Parse(commArg[1]);
+    if (currCommand == "Add")
+    {
+        numbers.Add(value);
+    }
+    else if (currCommand == "Remove")
+    {
+        numbers.Remove(value);
+    }
+    else if (currCommand == "Replace")
+    {
+        int replacement = int.Parse(commArg[2]);
+        int index = numbers.IndexOf(value);
+        if (numbers.Contains(value))
+        {
+            numbers.RemoveAt(index);
+            numbers.Insert(index, replacement);
+        }
+        
+    }
+    else if (currCommand == "Collapse")
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] < value)
+            {
+                numbers.Remove(numbers[i]);
+                i = -1;
+            }
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
