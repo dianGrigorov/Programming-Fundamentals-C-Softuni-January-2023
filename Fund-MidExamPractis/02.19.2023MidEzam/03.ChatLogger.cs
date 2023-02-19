6953971a6affa18fa97a9df51d3e4159fafb9469
dianGrigorov <diangrigorov@gmail.com>
2023-02-19 13:21:18 +0200

    Create 03.ChatLogger.cs

---
+++ Fund-MidExamPractis/02.19.2023MidEzam/03.ChatLogger.cs
@@ -0,0 +1,55 @@
+﻿List<string> massage = new List<string>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "end")
+{
+    string[] commArg = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string currComand = commArg[0];
+
+    if (currComand == "Chat")
+    {
+        string currMassage = commArg[1];
+        massage.Add(currMassage);
+    }
+    else if (currComand == "Delete")
+    {
+        string currMassage = commArg[1];
+        if (massage.Contains(currMassage))
+        {
+            massage.Remove(currMassage);
+        }
+    }
+    else if (currComand == "Edit")
+    {
+        string currMassage = commArg[1];
+        string editedMassage = commArg[2];
+        int index = massage.IndexOf(currMassage);
+        if (massage.Contains(currMassage))
+        {
+            massage.RemoveAt(index);
+            massage.Insert(index, editedMassage);
+        }
+    }
+    else if (currComand == "Pin")
+    {
+        string currMassage = commArg[1];
+        if (massage.Contains(currMassage))
+        {
+            massage.Remove(currMassage);
+            massage.Add(currMassage);
+        }
+        
+    }
+    else if (currComand == "Spam")
+    {
+        string currMassage = commArg[1];
+        string[] spam = input.Split(" ").Skip(1).ToArray();
+        for (int i = 0; i < spam.Length; i++)
+        {
+            massage.Add(spam[i]);
+        }
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, massage));
