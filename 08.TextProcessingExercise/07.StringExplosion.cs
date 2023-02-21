11dbbf75d77395d5ce49207ad7de0ca4968b1246
dianGrigorov <diangrigorov@gmail.com>
2023-02-21 20:26:43 +0200

    Create 07.StringExplosion.cs

---
+++ 08.TextProcessingExercise/07.StringExplosion.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Data.Common;
+using System.Text;
+
+string input = Console.ReadLine();
+
+//abv>1>1>2>2asdasd
+StringBuilder builder = new StringBuilder();
+
+int power = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+	char curr = (char)input[i];
+	if (curr == '>')
+	{
+		power += int.Parse(input[i + 1].ToString());
+		builder.Append(curr);
+	}
+	else if (power == 0)
+	{
+        builder.Append(curr);
+    }
+	else
+	{
+		power--;
+	}
+
+}
+
+Console.WriteLine(builder);
