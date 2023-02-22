abe85f44fd7afeeee57e6907d6b3f6ae22142332
dianGrigorov <diangrigorov@gmail.com>
2023-02-22 12:34:19 +0200

    Create 01.MatchFullName.cs

---
+++ 09.RegularExpressiom/01.MatchFullName.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+string tamplate = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+MatchCollection mathcedNames = Regex.Matches(input,tamplate);
+
+foreach(Match name in mathcedNames)
+{
+    Console.Write(name.Value + " ");
+}
