a2e6e8aa1f4d0810ce6983759d00fce90967b9ea
dianGrigorov <diangrigorov@gmail.com>
2023-02-22 15:32:52 +0200

    Create 02.MatchPoneNumber.cs

---
+++ 09.RegularExpressiom/02.MatchPoneNumber.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.RegularExpressions;
+
+string phoneNumber = Console.ReadLine();
+
+string pattern = @"\+\b359( |-)2\1\d{3}\1\d{4}\b";
+
+Regex regex = new Regex(pattern);
+MatchCollection matches = regex.Matches(phoneNumber);
+
+
+string[] result = matches.Select(x => x.Value).ToArray();
+
+Console.WriteLine(string.Join(", ", result));  
