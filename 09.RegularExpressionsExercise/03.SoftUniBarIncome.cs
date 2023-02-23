51e50b4794fb33e674d60cd7a35772b17cf1389a
dianGrigorov <diangrigorov@gmail.com>
2023-02-23 11:33:58 +0200

    Create 03.SoftUniBarIncome.cs

---
+++ 09.RegularExpressionsExercise/03.SoftUniBarIncome.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Text.RegularExpressions;
+
+string pattern = @"%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>[\w]+)>[^|$%.]*\|(?<count>[\d]+)\|[^|$%.]*?(?<price>\d+[.]?\d+)\$";
+Regex regex = new Regex(pattern);
+
+double totalSum = 0;
+string input = Console.ReadLine();
+
+while (input != "end of shift")
+{
+  Match match = regex.Match(input);
+
+	if (!match.Success)
+	{
+        input = Console.ReadLine();
+        continue;
+    }
+
+    string name = match.Groups["customer"].Value;
+    string product = match.Groups["product"].Value;
+    int count = int.Parse(match.Groups["count"].Value);
+    double price = double.Parse(match.Groups["price"].Value);
+
+    Console.WriteLine($"{name}: {product} - {count * price:f2}");
+    totalSum += count * price;
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"Total income: {totalSum:f2}");
