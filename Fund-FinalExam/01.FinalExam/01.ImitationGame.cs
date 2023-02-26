8c8caac4b68d8b0cfe00878866a86dcbc2201479
dianGrigorov <diangrigorov@gmail.com>
2023-02-26 18:07:14 +0200

    Create 01.ImitationGame.cs

---
+++ Fund-FinalExam/01.FinalExam/01.ImitationGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+string command;
+StringBuilder sb = new StringBuilder();
+sb.Append(input);
+while ((command = Console.ReadLine()) != "Decode")
+{
+    string[] commandArg = command.Split("|");
+    
+    string comm = commandArg[0];
+   
+    if (comm == "Move")
+    {
+        int subLenght = int.Parse(commandArg[1]);
+        string sub = sb.ToString().Substring(0, subLenght);
+        sb.Remove(0, subLenght);
+        sb.Append(sub);
+       
+        
+    }
+    else if (comm == "Insert")
+    {
+        int index = int.Parse(commandArg[1]);
+        string value = commandArg[2];
+        sb.Insert(index, value);
+
+
+    }
+    else if (comm == "ChangeAll")
+    {
+        string subStr = commandArg[1];
+        string replacement = commandArg[2];
+
+        sb.Replace(subStr, replacement);
+        
+    }
+}
+Console.WriteLine($"The decrypted message is: {sb}");
