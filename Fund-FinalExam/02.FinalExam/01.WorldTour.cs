153b19a3fd92546a214a644259c48d83fb05d2a7
dianGrigorov <diangrigorov@gmail.com>
2023-02-28 09:32:30 +0200

    Create 01.WorldTour.cs

---
+++ Fund-FinalExam/02.FinalExam/01.WorldTour.cs
@@ -0,0 +1,44 @@
+﻿string road = Console.ReadLine();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Travel")
+{
+    string[] commandArg = input.Split(":");
+    string curCommand = commandArg[0];
+
+    if (curCommand == "Add Stop")
+    {
+        int index = int.Parse(commandArg[1]);
+        string place = commandArg[2];
+
+        road = road.Insert(index, place);
+        Console.WriteLine(road);
+    }
+    else if (curCommand == "Remove Stop")
+    {
+        int startIndex = int.Parse(commandArg[1]);
+        int endIndex = int.Parse(commandArg[2]);
+
+
+        if (startIndex > 0 && startIndex < road.Length && endIndex > 0 && endIndex < road.Length)
+        {
+            int count = endIndex - startIndex + 1;
+            road = road.Remove(startIndex, count);
+            Console.WriteLine(road);
+        }
+    }
+    else if (curCommand == "Switch")
+    {
+        string oldString = commandArg[1];
+        string newString = commandArg[2];
+
+        if (road.Contains(oldString))
+        {
+            road = road.Replace(oldString, newString);
+            Console.WriteLine(road);
+        }
+    }
+
+}
+Console.WriteLine($"Ready for world tour! Planned stops: {road}");
