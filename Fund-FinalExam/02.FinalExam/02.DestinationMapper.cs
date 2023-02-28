6262a8ce570bbaa1fd0d928c8d8836b0fcc42455
dianGrigorov <diangrigorov@gmail.com>
2023-02-28 10:06:38 +0200

    Create 02.DestinationMapper.cs

---
+++ Fund-FinalExam/02.FinalExam/02.DestinationMapper.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+string pattern = @"(=|/)(?<place>[A-Za-z]{3,})\1";
+
+List<string> places = new List<string>();
+
+MatchCollection matches = Regex.Matches(input, pattern);
+
+int sum = 0;
+
+foreach (Match match in matches)
+{
+    places.Add(match.Groups["place"].Value);
+    sum += match.Groups["place"].Value.Length;
+}
+Console.WriteLine($"Destinations: {string.Join(", ", places)}");
+Console.WriteLine($"Travel Points: {sum}");
