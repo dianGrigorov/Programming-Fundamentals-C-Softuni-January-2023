3aa40d9d12f63309884fc29c5d40860f66d30e11
dianGrigorov <diangrigorov@gmail.com>
2023-03-02 12:34:29 +0200

    Create 02.MirrorWords.cs

---
+++ Fund-FinalExam/03FinalExam/02.MirrorWords.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+
+string pattern = @"(#|@)(?<firstWords>[A-Za-z]{3,})\1{2}(?<secondWord>[A-Za-z]{3,})\1";
+
+MatchCollection matchCollection = Regex.Matches(text, pattern);
+List<string> matchedWords = new List<string>();
+
+foreach (Match match in matchCollection)
+{
+    string firstWord = match.Groups["firstWords"].Value;
+    string secondWord = match.Groups["secondWord"].Value;
+
+    if (firstWord == new string(secondWord.Reverse().ToArray()))
+    {
+        string matches = firstWord + " <=> " + secondWord;
+        matchedWords.Add(matches);
+       
+    }
+}
+if (matchCollection.Count == 0)
+{
+    Console.WriteLine("No word pairs found!");
+}
+else
+{
+    Console.WriteLine($"{matchCollection.Count} word pairs found!");
+}
+
+if (matchedWords.Count == 0)
+{
+    Console.WriteLine("No mirror words!");
+}
+else
+{
+    Console.WriteLine("The mirror words are:");
+    Console.WriteLine(string.Join(", ", matchedWords));
+}
