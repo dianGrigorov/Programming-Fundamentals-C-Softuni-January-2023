e08649671486362ee0a1c4f1446d4cf62cf28a3a
dianGrigorov <diangrigorov@gmail.com>
2023-03-02 12:34:27 +0200

    Create 01.SecretChat.cs

---
+++ Fund-FinalExam/03FinalExam/01.SecretChat.cs
@@ -0,0 +1,58 @@
+﻿
+
+string message = Console.ReadLine();
+
+string info;
+
+while ((info = Console.ReadLine()) != "Reveal")
+{
+    string[] commArg = info.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+
+    string currComm = commArg[0];
+
+    if (currComm == "InsertSpace")
+    {
+        int index = int.Parse(commArg[1]);
+        message = message.Insert(index, " ");
+        Console.WriteLine(message);
+    }
+    else if (currComm == "Reverse")
+    {
+        string subString = commArg[1];
+
+        if (message.Contains(subString))
+        {
+
+            //char[] subChars = subString.ToCharArray();
+            //subChars.Reverse();
+            //Array.Reverse(subChars);
+            string reversed = new string(subString.Reverse().ToArray());
+
+            int index = message.IndexOf(subString);
+            message = message.Remove(index, subString.Length);
+            message += reversed;
+            Console.WriteLine(message);
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+    }
+    else if (currComm == "ChangeAll")
+    {
+        string oldStr = commArg[1];
+        string newStr = commArg[2];
+
+        if (message.Contains(oldStr))
+        {
+            message = message.Replace(oldStr, newStr);
+            Console.WriteLine(message);
+        }
+        else
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
+Console.WriteLine($"You have a new text message: {message}");
