dd97d17a0a979e00dd84b153e3920e84cee28283
dianGrigorov <diangrigorov@gmail.com>
2023-03-06 08:06:22 +0200

    Create 01.ActivationKey.cs

---
+++ Fund-FinalExam/05.FinalExam/01.ActivationKey.cs
@@ -0,0 +1,54 @@
+﻿
+string rawKey = Console.ReadLine();
+
+string command;
+
+while ((command = Console.ReadLine()) != "Generate")
+{
+    string[] commArg = command.Split(">>>");
+    string currCom = commArg[0];
+
+    if (currCom == "Contains")
+    {
+        string substring = commArg[1];
+        if (rawKey.Contains(substring))
+        {
+            Console.WriteLine($"{rawKey} contains {substring}");
+        }
+        else
+        {
+            Console.WriteLine("Substring not found!");
+        }
+    }
+    else if (currCom == "Flip")
+    {
+        string upperOrLower = commArg[1];
+        int startIndex = int.Parse(commArg[2]);
+        int endIndex = int.Parse(commArg[3]);
+        int lenght = endIndex - startIndex;
+
+        if (upperOrLower == "Upper")
+        {
+            string subString = rawKey.Substring(startIndex, lenght);
+            string upperSubString = subString.ToUpper();
+            rawKey = rawKey.Replace(subString, upperSubString);
+        }
+        else if (upperOrLower == "Lower")
+        {
+            string subString = rawKey.Substring(startIndex, lenght);
+            string lowerSubString = subString.ToLower();
+            rawKey = rawKey.Replace(subString, lowerSubString);
+            
+        }
+        Console.WriteLine(rawKey);
+    }
+    else if (currCom == "Slice")
+    {
+        int startIndex = int.Parse(commArg[1]);
+        int endIndex = int.Parse(commArg[2]);
+        int lenght = endIndex - startIndex;
+        rawKey = rawKey.Remove(startIndex, lenght);
+        Console.WriteLine(rawKey);
+    }
+}
+Console.WriteLine($"Your activation key is: {rawKey}");
