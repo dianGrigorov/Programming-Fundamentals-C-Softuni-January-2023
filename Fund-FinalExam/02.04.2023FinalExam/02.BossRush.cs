92190e1e9e2e327b2ea11a562159b4b7259c81bc
dianGrigorov <diangrigorov@gmail.com>
2023-04-02 12:48:47 +0300

    Create 02.BossRush.cs

---
+++ Fund-FinalExam/02.04.2023FinalExam/02.BossRush.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Text.RegularExpressions;
+
+int n = int.Parse(Console.ReadLine());
+string pattern = @"\|(?<name>[A-Z]{4,})\|:#(?<title>[A-Za-z]+\s[A-Za-z]+)#";
+
+Regex regex = new Regex(pattern);
+Dictionary<string, string> bosses = new Dictionary<string, string>(); 
+for (int i = 0; i < n; i++)
+{
+    string token = Console.ReadLine();
+    Match matches = regex.Match(token);
+    if (matches.Success)
+    {
+        string name = matches.Groups["name"].Value;
+        string title = matches.Groups["title"].Value;
+        Console.WriteLine($"{name}, The {title}");
+        Console.WriteLine($">> Strength: {name.Length}");
+        Console.WriteLine($">> Armor: {title.Length}");
+    }
+    else
+    {
+        Console.WriteLine("Access denied!");
+    }
+}
